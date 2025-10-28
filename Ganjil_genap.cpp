97b8193c50b4b0b5e962dc7d0ba342d5db37b704
farhtwfdlloh <farhatwafdulloh3@gmail.com>
2025-10-28 11:28:25 +0700

    Menambahkan c++ Ganjil atau genap

---
+++ Ganjil_genap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  int bilangan;
+
+  cout << "Masukkan sebuah bilangan: ";
+  cin >> bilangan; 
+  
+  if (bilangan % 2 == 0) {
+    cout << "Bilangan tersbeut adalah bilangan GENAP" << endl;
+  } else {
+    cout << "Bilangan tersebut adalah bilangan GANJIL" << endl;
+  }
+return 0;
+}
