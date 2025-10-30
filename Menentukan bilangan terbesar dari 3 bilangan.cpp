8da0d2c53e7103c3b3628b3d8061872046099d98
abdee23437 <322510015@student.machung.co.id>
2025-10-30 22:21:55 +0700

    menambahkan

---
+++ Menentukan bilangan terbesar dari 3 bilangan.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, terbesar;
+
+    cout << "Masukkan bilangan pertama: ";
+    cin >> A;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> B;
+    cout << "Masukkan bilangan ketiga: ";
+    cin >> C;
+
+    if (A > B && A > C)
+        terbesar = A;
+    else if (B > A && B > C)
+        terbesar = B;
+    else
+        terbesar = C;
+
+    cout << "Bilangan terbesar adalah: " << terbesar;
+    
+    return 0;
+}
